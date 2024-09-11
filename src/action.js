4e0ae0b910d4d798d79aba1226af4c0e17d7e8d0
Rihanrh <rihanh@student.telkomuniversity.ac.id>
2024-09-11 20:22:00 +0700

    Change to new actions

--- src/action.js
+++ src/action.js
@@ -2,18 +2,36 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    try {
+        const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+        const branchName = core.getInput('branch_name');
+        const baseBranch = core.getInput('base_branch');
 
-    const octokit = github.getOctokit(GITHUB_TOKEN);
+        const octokit = github.getOctokit(GITHUB_TOKEN);
+        const { context = {} } = github;
+        const { owner, repo } = context.repo;
 
-    const { context = {} } = github;
-    const { pull_request } = context.payload;
+        // Get the SHA of the base branch
+        const { data: refData } = await octokit.rest.git.getRef({
+            owner,
+            repo,
+            ref: `heads/${baseBranch}`,
+        });
+        const sha = refData.object.sha;
 
-    await octokit.rest.issues.createComment({
-        ...context.repo,
-        issue_number: pull_request.number,
-        body: 'Pull request has been created'
-    });
+        // Create a new branch
+        await octokit.rest.git.createRef({
+            owner,
+            repo,
+            ref: `refs/heads/${branchName}`,
+            sha: sha,
+        });
+
+        console.log(`Successfully created branch: ${branchName}`);
+        core.setOutput("branch_name", branchName);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
