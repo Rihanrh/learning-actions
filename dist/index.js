8872bbdb490f21121e67ba14390564bd9a91cbea
Rihanrh <rihanh@student.telkomuniversity.ac.id>
2024-09-11 20:29:02 +0700

    Update index.js using yarn build

--- dist/index.js
+++ dist/index.js
@@ -31095,18 +31095,36 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
 async function run() {
-    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    try {
+        const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+        const branchName = core.getInput('branch_name');
+        const baseBranch = core.getInput('base_branch');
 
-    const octokit = github.getOctokit(GITHUB_TOKEN);
+        const octokit = github.getOctokit(GITHUB_TOKEN);
+        const { context = {} } = github;
+        const { owner, repo } = context.repo;
 
-    const { context = {} } = github;
-    const { pull_request } = context.payload;
+        // Get the SHA of the base branch
+        const { data: refData } = await octokit.rest.git.getRef({
+            owner,
+            repo,
+            ref: `heads/${baseBranch}`,
+        });
+        const sha = refData.object.sha;
 
-    await octokit.rest.issues.createComment({
-        ...context.repo,
-        issue_number: pull_request.number,
-        body: 'Pull request has been created'
-    });
+        // Create a new branch
+        await octokit.rest.git.createRef({
+            owner,
+            repo,
+            ref: `refs/heads/${branchName}`,
+            sha: sha,
+        });
+
+        console.log(`Successfully created branch: ${branchName}`);
+        core.setOutput("branch_name", branchName);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
